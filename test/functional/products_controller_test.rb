28ae8731f43c0174498ce3093f307f246563ece5
isp-mbp <1334@shadowproject.net>
2012-03-02 08:40:41 +0100

    fixed first tests

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,13 +1,21 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
-    @upadte = {
-      title:        'Lorem Ipsum',
-      description:  'Wibbles are fun!',
-      image_url:    'lorem.png',
-      price:        19.95
+    @update = {
+      title:       'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url:   'lorem.jpg',
+      price:       19.95
     }
   end
 
@@ -24,11 +32,13 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: @upadte
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(assigns(:product))
   end
+
+  # ...
 
   test "should show product" do
     get :show, id: @product
@@ -44,6 +54,8 @@ class ProductsControllerTest < ActionController::TestCase
     put :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
+
+  # ...
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
